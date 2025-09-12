baf8bea47ae2d6a5ddd74bba5d4a628c418a5bf7
Deepankar Gupta <deepankarggupta@gmail.com>
2025-09-13 01:00:28 +0530

    Create Xcode Project

---
+++ BookFinder/BookFinder/BookFinderApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookFinderApp.swift
+//  BookFinder
+//
+//  Created by Deepankar Gupta on 13/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BookFinderApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
